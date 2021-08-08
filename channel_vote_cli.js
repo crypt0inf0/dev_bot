3474b3ea8513908cce79c4349a50dbe4d8b1c6c3
crypt0inf0 <cryptoinfo03@gmail.com>
2021-08-08 13:12:57 +0530

    vote all contents on channel

---
+++ channel_vote_cli.js
@@ -0,0 +1,33 @@
+const javalon = require('javalon')
+
+const username = ''
+const priv_key = ''
+
+
+javalon.getDiscussionsByAuthor(process.argv[2], null, null, (err, contents) => {
+    // console.log(err, contents)
+    var content_count = Object.keys(contents).length;
+    
+    for (i = 0; i < content_count; i++) {
+    let author = contents[i].author
+    let permlink = contents[i].link
+    let vp = -250
+    let tag = ''
+    
+    var newTx = {
+        type: javalon.TransactionType.VOTE,
+        data: {
+            author: author,
+            link: permlink,
+            vt: vp,
+            tag: tag
+        }
+      }
+    // console.log(newTx)
+    newTx = javalon.sign(priv_key, username, newTx)
+    
+    javalon.sendTransaction(newTx, function(err, res) {
+        cb(err, res)
+    })
+    }
+});
