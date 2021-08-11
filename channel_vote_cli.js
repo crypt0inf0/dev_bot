3518d622bb4c84f00b1e552e9277b84b3cfc2779
crypt0inf0 <cryptoinfo03@gmail.com>
2021-08-11 15:45:44 +0530

    added yargs

--- channel_vote_cli.js
+++ channel_vote_cli.js
@@ -1,18 +1,28 @@
 const javalon = require('javalon')
+const yargs = require("yargs")
+
 const cb = (err, res) => console.log("Error: ", err, "Result: ", res)
+
+const options = yargs
+  .usage("Usage: -u <username> -t <tag (optional)> -m <minimum vp> -x <maximum vp>")
+  .option("u", { alias: "username", describe: "dtube username", type: "string", demandOption: true })
+  .option("t", { alias: "tag", describe: "tag your vote", type: "string" })
+  .option('m', { alias: "min_vp", describe: "minimum vp to vote", default: "100", type: "number", demandOption: true })
+  .option('x', { alias: "max_vp", describe: "maximum vp to vote", default: "1000", type: "number", demandOption: true })
+.argv;
 
 const username = ''
 const priv_key = ''
 
 
-javalon.getDiscussionsByAuthor(process.argv[2], null, null, (err, contents) => {
+javalon.getDiscussionsByAuthor(options.username, null, null, (err, contents) => {
     // console.log(err, contents)
     var content_count = Object.keys(contents).length;
     
     for (i = 0; i < content_count; i++) {
     let author = contents[i].author
     let permlink = contents[i].link
-    let vp = -250 // Change vp | Math.floor(Math.floor(Math.random() * 2048) - 2050); // Random num between -2-2050
+    let vp = Math.floor(Math.random() * (options.max_vp - options.min_vp + 1) + options.min_vp) // | vp = 246
     let tag = ''
     
     var newTx = {
