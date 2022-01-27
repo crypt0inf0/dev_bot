221767f34eda8d08f6cdc227c69123cdd48cf653
crypt0inf0 <84570125+crypt0inf0@users.noreply.github.com>
2022-01-27 12:36:16 +0530

    fix validate db

--- post.js
+++ post.js
@@ -1,7 +1,8 @@
+const fs = require('fs')
 const dotenv = require('dotenv')
 const javalon = require('javalon')
 const youtubedl = require('youtube-dl')
-const { getChannelFeed } = require('@obg-lab/youtube-channel-feed');
+const { getChannelFeed } = require('@obg-lab/youtube-channel-feed')
 
 dotenv.config( { path: '.env'} )
 const cb = (err, res) => console.log("Error: ", err, "Result: ", res)
@@ -9,111 +10,107 @@ const cb = (err, res) => console.log("Error: ", err, "Result: ", res)
 const username = process.env.USERNAME;
 const priv_key = process.env.PRIV_KEY;
 
-// Get channel id by finding "externalId" on page source
-const channel_id = 'UCXuqSBlHAE6Xw-yeJA0Tunw';
+const channel_id = 'UCDR8cvjALazMm2j9hOar8_g';
 const options = ['--username=user', '--password=hunter2']
 
-validateData(); 
+function intervalFunc() {
+	validate()
+}setInterval(intervalFunc, 60000);
 
-// Get youtube channel feed
-async function getYoutubefeed() {
-	try {
-		const feed = await getChannelFeed(channel_id)
-		return feed
-	} catch (error) {
-		console.log(error);
+
+async function validate() {
+	// Fetch youtube feed
+	const channel_feed = await getChannelFeed(channel_id)
+	video_id = await channel_feed.feed.entry[0].id[0].split(':')[2];
+
+	// Check video id's against the new video id
+	var data = fs.readFileSync('./db/youtube.json');
+	var ids = JSON.parse(data);
+				
+	var youtube_data = {
+		id: video_id
 	}
-}
-
-// Get latest video id
-async function getVideoid() {
-	let channel_feed = await getYoutubefeed();
-	let video_id = channel_feed.feed.entry[0].id[0].split(':')[2];
-	return video_id
-}
-
-// Validate data
-async function validateData() {
-	try{
-		let video_id = await getVideoid();
-		// console.log(video_id)
-		
-		// Check video id against the new video id
-		javalon.getDiscussionsByAuthor('clipbytes01', null, null, (err, contents) => {
-			let dtube_id = contents[0].json.files.youtube;
-			if (video_id === dtube_id) {
-				console.log("No new video found")
-				return
-			} else {
-				// Grab video info & post function
-                		getVideoinfo()
-            		}
-        	})
-    } catch (error) {
-		console.log(error);
-	}
-}
-
-async function getVideoinfo() {
-	let video_id = await getVideoid();
-	let url = 'https://youtu.be/' + video_id;
-	// Grab youtube video info
-	youtubedl.getInfo(url, options, function(err, info) {
-		if (err) throw err
-
-		const youtube_id = info.id;
-		const title = info.title;
-		const description = info.description;
-		const duration = info._duration_raw;
-		const tag = '';
-
-		autoPost()
-
-		// posting
-		function autoPost() {
-			console.log('Posting...', )
-			try {
-				var vp = Math.floor(Math.floor(Math.random() * 1500) + 500); // Random num between 500-1500
-				// var tag = '';
-				// Generate random link
-				function generatePermlink() {
-					let permlink = ""
-					let possible = "abcdefghijklmnopqrstuvwxyz0123456789"
-					for (let i = 0; i < 8; i++)
-						permlink += possible.charAt(Math.floor(Math.random() * possible.length))
-					return permlink
+	
+	// Validate & add data
+	function addDb(youtube_data) {
+  		var index = ids.findIndex(o => o.id === video_id)
+  		if (index === -1) {
+			post()
+			console.log("New video found.")
+			ids.push(youtube_data);
+			fs.writeFileSync("./db/youtube.json", JSON.stringify(ids, null, 2), 'utf8'), (err) => {
+				if (err) {  console.error(err);  return; };
+					console.log("Updating DB...");
 				}
-				var postLink = generatePermlink()
-				console.log(postLink)
-				// Broadcast vote to blockchain
-				var newTx = {
-					type: javalon.TransactionType.COMMENT,
-					data: {
-						link: postLink,
-						json: {
-							files: {
-								youtube: youtube_id
-							},
-							title: title,
-							desc: description,
-							dur: duration,
-							tag: tag,
-							refs: []
-						},
-						vt: vp,
-						tag: tag
-					}
-				}
-				// Sign transaction
-				newTx = javalon.sign(priv_key, username, newTx)
-				// console.log(newTx)
-				// Send transaction to blockchain
-				javalon.sendRawTransaction(newTx, function(err, res) {
-				    cb(err, res)
-				})
-			} catch (error) {
-				console.error(error);
-			}
+		}else {
+			console.log("Video already exists" + " " + video_id)
 		}
-	})
+	}
+	addDb(youtube_data);
+}
+
+
+function post() {
+        //Grab video info & post function
+        let url = 'https://youtu.be/' + video_id;
+
+        youtubedl.getInfo(url, options, function(err, info) {
+                if (err) throw err
+
+                youtube_id = info.id;
+                title = info.title;
+                description = info.description;
+                duration = info._duration_raw;
+                tag = 'promo';
+
+                autoPost()
+        })
+}
+
+// posting
+function autoPost() {
+        console.log('Posting...', )
+        console.log(youtube_id)
+        try {
+	        var vp = Math.floor(Math.floor(Math.random() * 50) + 50); // Random num between 500-1500
+                // var tag = '';
+                // Generate random link
+                function generatePermlink() {
+    	                let permlink = ""
+                        let possible = "abcdefghijklmnopqrstuvwxyz0123456789"
+                        for (let i = 0; i < 8; i++)
+                        permlink += possible.charAt(Math.floor(Math.random() * possible.length))
+                        return permlink
+                }
+                var postLink = generatePermlink()
+                //console.log(postLink)
+                // Broadcast vote to blockchain
+                var newTx = {
+                        type: javalon.TransactionType.COMMENT,
+                        data: {
+                                link: postLink,
+                                json: {
+                                        files: {
+                                                 youtube: youtube_id
+                                        },
+                                        title: title,
+                                        desc: description,
+                                        dur: duration,
+                                        tag: tag,
+                                        refs: []
+                                },
+                                vt: vp,
+                                tag: tag
+                        }
+                }
+                // Sign transaction
+                newTx = javalon.sign(priv_key, username, newTx)
+                // console.log(newTx)
+                // Send transaction to blockchain
+                javalon.sendRawTransaction(newTx, function(err, res) {
+                        cb(err, res)
+                })
+        } catch (error) {
+                console.error(error);
+        }
 }
