4f12a05bb32520fcc1055a4498ee82ac2edd9c78
crypt0inf0 <84570125+crypt0inf0@users.noreply.github.com>
2022-02-04 21:27:00 +0530

    Create claimrewards.js

---
+++ claimrewards.js
@@ -0,0 +1,46 @@
+const javalon = require('javalon')
+const cb = (err, res) => console.log("Error: ", err, "Result: ", res)
+
+const username = process.env.USERNAME;
+const priv_key = process.env.PRIV_KEY;
+
+function intervalFunc() {
+	claimReward();
+}setInterval(intervalFunc, 5000);
+
+function claimReward() {
+	javalon.getClaimableVotesByAccount(username, 0, (err, votes) => {
+    	// console.log(err, votes)
+		if (votes.length == '0'){
+		  return;
+		} else{
+			author = votes[0].author
+			link = votes[0].link
+		}
+		
+		post();
+		
+	})
+}
+
+function post() {
+	// Broadcast vote to blockchain
+	try{
+		var newTx = {
+				type: javalon.TransactionType.CLAIM_REWARD,
+				data: {
+					author: author,
+					link: link
+				}
+		}
+    // Sign transaction
+    newTx = javalon.sign(priv_key, username, newTx)
+    // console.log(newTx)
+    // Send transaction to blockchain
+    javalon.sendTransaction(newTx, function(err, res) {
+      cb(err, res)
+    })
+	} catch (error) {
+    	console.error(error);
+  }
+}
